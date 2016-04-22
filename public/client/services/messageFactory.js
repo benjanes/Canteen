183f2e7323a01ca1d25ae6c9a44b243c27b461b3
Ben Janes <brjanes@gmail.com>
2016-04-22 08:36:04 -0400

    Remove comments from messageFactory

--- public/client/services/messageFactory.js
+++ public/client/services/messageFactory.js
@@ -30,37 +30,6 @@ angular.module('canteen.messageFactory', [])
       });
     }
 
-    // function getTrip() {
-    //   return $http({
-    //     method: 'GET',
-    //     url: '/api/trip',
-    //   })
-    //   .then(function (resp) {
-    //     if(!resp.data._id){
-    //       $location.path('/#/landing-page');
-    //       window.location.reload(true);
-    //     }
-    //     return resp.data;
-    //   })
-    //   .catch(function (err) {
-    //     console.error(err);
-    //   });
-    // }
-
-    // function updateStatus(trip) {
-    //   return $http({
-    //     method: 'PUT',
-    //     url: '/api/trip/',
-    //     data: trip,
-    //   })
-    //   .then(function (resp) {
-    //     return resp.data;
-    //   })
-    //   .catch(function (err) {
-    //     console.error(err);
-    //   });
-    // }
-
     // Factory methods use promises
     return {
       getMessages: getMessages,
